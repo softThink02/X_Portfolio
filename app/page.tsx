b49313a9f29600fea15fb7dddd0b638c2844717e
SThink <softthink02@gmail.com>
2025-08-13 16:18:36 +0200

    [removed the feedback from clients section]

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
+// import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -28,7 +28,7 @@ const Home = () => {
         <Hero />
         <Grid /> 
         <RecentProjects />
-        <Clients />
+        {/* <Clients /> */}
         <Experience />
         <Footer />
       </div>
