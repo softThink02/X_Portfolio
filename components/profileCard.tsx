b4ae4a50f3db25046987a6a653fc0b5f362bc3cb
SThink <softthink02@gmail.com>
2025-10-02 17:51:38 +0200

    []

--- components/profileCard.tsx
+++ components/profileCard.tsx
@@ -45,24 +45,24 @@ export default function ProfileCard() {
         <div className="flex items-center">
           <MagicButton
             title="Hire Me"
-            icon={<FaLocationArrow />}
+            icon={<FaLocationArrow size={12} />}
             position="right"
           />
         </div>
 
         <div
-          className={`flex items-center justify-center bg-black-200 text-green-700 h-[60px] rounded-full relative overflow-hidden w-[230px]`}
+          className={`flex items-center justify-center bg-black-200 text-green-700 h-[60px] rounded-full relative overflow-hidden w-[220px] md:w-[250px] lg:w-[300px] xl:w-[350px]`}
         >
           <AnimatePresence mode="wait">
             {showText ? (
-              <FooterHeader variant="heroDiv" className="absolute inset-0 flex text-[14px] items-center justify-center">
+              <FooterHeader variant="heroDiv" className="absolute inset-0 flex text-[12px] font-bold md:text-[14px] lg:text-[16px] xl:text-[20px] items-center justify-center">
                 <span className="w-3 h-3 bg-green-500 rounded-full mr-2"></span>
                 Available for new project
               </FooterHeader>
             ) : (
               <FooterHeader
                 variant="heroImg"
-                className="absolute inset-0 flex items-center justify-center text-2xl"
+                className="absolute inset-0 flex items-center justify-center text-[20px] lg:text-[24px] xl:text-[32px]"
               >
                 ✈️
               </FooterHeader>
