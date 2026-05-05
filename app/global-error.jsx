8c81628cec47448350b8b6249325302acc47942b
X <softthink02@gmail.com>
2026-05-05 23:03:05 +0100

    [added sentry for vitual error handling]

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -3,10 +3,14 @@
 import Error from "next/error";
 import { useEffect } from "react";
 
+import * as Sentry from "@sentry/nextjs";
+
 export default function GlobalError({ error }) {
   useEffect(() => {
     console.error(error);
   }, [error]);
+
+  Sentry.captureException(error);
 
   return (
     <html>
---
+++ instrumentation-client.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f9582dc2dac55848b774291c0a70c5a2@o4509293567934464.ingest.us.sentry.io/4511339433951232",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
